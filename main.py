9d9cdbf938f70f1e9864d98b943f0b6d7de09449
mr-papu-jpg <angstart.1109@gmail.com>
2026-01-17 16:11:45 -0400

    feat: Create the basic crud functions and use Path and Query

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
+
+from starlette.types import HTTPExceptionHandler
 
 app= FastAPI()
 
 
 class Heroe(BaseModel):
+    id: int
     nombre: str
     super_poder: str
     nivel_fuerza: int
@@ -17,6 +20,9 @@ base_de_datos_heroes= []
 
 @app.post("/heroes/")
 def crear_juego(heroe: Heroe):
+    if  heroe[nivel_fuerza] > 100:
+        raise HTTPException(status_code=400, detail="La fuerza debe ser menor de 100")
+
     base_de_datos_heroes.append(heroe.dict())
     return {"mensaje":"heroe guardado", "datos": heroe}
 
@@ -24,3 +30,25 @@ def crear_juego(heroe: Heroe):
 @app.get("/heroes/")
 def listar_heroes():
     return base_de_datos_heroes
+
+@app.get("/poder/")
+def listar_poder(poder: str= None):
+    if poder: 
+        resultados= [u for u in base_de_datos_heroes if u[poder].lower() == poder.lower()]
+        return resultados
+    return base_de_datos_heroes
+
+@app.get("/poder/{id}")
+def obtener_poder(heroe_id: int):
+    for p in base_de_datos_heroes:
+        if p[id] == heroe_id:
+            return p 
+        raise HTTPException(status_code=404, detail="Heroe no encontrado")
+
+
+@app.delete("/heroes/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_heroe(id: int):
+    if id not in base_de_datos_heroes:
+        raise HTTPException(status_code=404, detail="ID no encontrado")
+    del base_de_datos_heroes[id]
+    return {"mensaje":f"Heroe {id} fue eliminado."}
