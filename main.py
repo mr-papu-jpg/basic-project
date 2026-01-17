6e5fd112b05ed0093a715999ca01a7ae58be84c7
mr-papu-jpg <angstart.1109@gmail.com>
2026-01-17 15:34:37 -0400

    feat: Modify and create new logic for the main

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app= FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "Servidor corriendo"}
 
-@app.get("/multiplicar/{numero}")
-def multiplicar(numero: int):
-    return {"resultado": numero * 2}
+class Heroe(BaseModel):
+    nombre: str
+    super_poder: str
+    nivel_fuerza: int
+    esta_activo: Optional[bool]= True
+
+
+base_de_datos_heroes= []
+
+
+@app.post("/heroes/")
+def crear_juego(heroe: Heroe):
+    base_de_datos_heroes.append(heroe.dict())
+    return {"mensaje":"heroe guardado", "datos": heroe}
+
+
+@app.get("/heroes/")
+def listar_heroes():
+    return base_de_datos_heroes
